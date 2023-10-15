86fc8162acf2adf20617c52dafa77211d1db89af
mij <mijsmatthew@gmail.com>
2023-10-15 15:40:31 -0700

    added styling, added desc text.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png'
 import CortanaImage from './images/cortana.png'
@@ -9,11 +10,48 @@ import SiriImage from './images/siri.png'
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>        
+                </div>
+            </section>
+            
 
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} alt=""/>
-            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} alt=""/>
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} alt=""/>
+            <div className='container'>
+                <div className='section'>
+                    <div className='columns'>
+                        <div className='column is-3'>
+                            <ProfileCard
+                            title="Alexa"
+                            handle="@alexa99"
+                            image={AlexaImage}
+                            alt=""
+                            description="Alexa was made by Amazon."
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                            title="Cortana" 
+                            handle="@cortana32" 
+                            image={CortanaImage} 
+                            alt=""
+                            description="Cortana was made by Microsoft."
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                            title="Siri" 
+                            handle="@siri01" 
+                            image={SiriImage} 
+                            alt=""
+                            description="Siri was made by Apple."
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
         </div>
     )
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,20 @@
 //use destructuring in argument
-function ProfileCard({title, handle, image, alt}) {
+function ProfileCard({title, handle, image, alt, description}) {
     return (
-        <div>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
-            <img src={image} alt={alt}/>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={alt}/>
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">{description}</div>
+            </div>
+            
         </div>
     );
 }
